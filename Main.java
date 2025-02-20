e9395dea656b600da7a5e6c1a320fef5674e2063
Zachcroteau <zachcroteau@gmail.com>
2025-02-20 20:41:28 +0000

    Random

--- Main.java
+++ Main.java
@@ -1,39 +1,37 @@
 import java.util.Scanner;
 
-public class Main public static void main(String[] args)
-  	{
+public class Main {
+	public static void main(String[] args) {
 		CaffeinatedBeverage[] inventory = new CaffeinatedBeverage[10];
 		Scanner keyboard = new Scanner(System.in);
 		// Prompt user with options in a loop
 		int choice, ounces, brewTemp, count = 0;
 		String name;
 		double price;
-		
-		do
-    	{
+
+		do {
 			System.out.println("1) Enter new Tea");
 			System.out.println("2) Enter new Yerba Mate");
 			System.out.println("3) Exit");
 			System.out.print(">>");
 			choice = keyboard.nextInt();
-			
-			switch (choice)
-			{
-			    case 1: // Tea
-			        System.out.print("Enter name      : ");
-			        name = keyboard.nextLine();
-			        System.out.print("Enter ounces    : ");
-			        ounces = keyboard.nextInt();
-			        System.out.print("Enter price     $ ");
-			        price = keyboard.nextDouble();
-			        System.out.print("Enter brew temperature (in Celsius): ");
-			        brewTemp = keyboard.nextInt();
-			
-			        // Create a Tea, put into array
-			        inventory[count] = new Tea(name, ounces, price, brewTemp);
+
+			switch (choice) {
+				case 1: // Tea
+					System.out.print("Enter name      : ");
+					name = keyboard.nextLine();
+					System.out.print("Enter ounces    : ");
+					ounces = keyboard.nextInt();
+					System.out.print("Enter price     $ ");
+					price = keyboard.nextDouble();
+					System.out.print("Enter brew temperature (in Celsius): ");
+					brewTemp = keyboard.nextInt();
+
+					// Create a Tea, put into array
+					inventory[count] = new Tea(name, ounces, price, brewTemp);
 
 					System.out.println("Your tea order has been added: " + inventory[count]);
-			        count++;
+					count++;
 					break;
 				case 2:
 					System.out.print("Enter name      : ");
@@ -48,12 +46,11 @@ public class Main public static void main(String[] args)
 					System.out.println("Your Yerba Mate order has been added: " + inventory[count]);
 					count++;
 					break;
-				
-			
+
 			}
 
 		} while (choice != 3);
-		
+
 		if (choice == 3) {
 			for (int i = 0; i <= inventory.length; i++) {
 				System.out.println(inventory.toString());
@@ -61,7 +58,6 @@ public class Main public static void main(String[] args)
 			System.out.println(findHighestPricedYerbaMate(inventory));
 		}
 	}
-}
 
 	public static YerbaMate findHighestPricedYerbaMate(CaffeinatedBeverage[] inventory) {
 		double max;
@@ -72,5 +68,6 @@ public class Main public static void main(String[] args)
 				highestPricedMate = inventory[i];
 			}
 		}
-	
+
+	}
 }
